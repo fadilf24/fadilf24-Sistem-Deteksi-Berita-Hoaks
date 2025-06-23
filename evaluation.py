9429cd4d33974c4341a49ab54536b534f57c4baa
Fadil Farhan Ridwan <98074501+fadilf24@users.noreply.github.com>
2025-06-23 22:16:31 +0700

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -1,4 +1,9 @@
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import (
+    accuracy_score, precision_score, recall_score, f1_score,
+    classification_report, confusion_matrix
+)
 
 def evaluate_model(y_true, y_pred):
     """
@@ -16,3 +21,17 @@ def generate_classification_report(y_true, y_pred, target_names=None):
     Menghasilkan classification report dalam bentuk string.
     """
     return classification_report(y_true, y_pred, target_names=target_names, zero_division=0)
+
+def plot_confusion_matrix(y_true, y_pred, labels, figsize=(5,4)):
+    """
+    Menampilkan confusion matrix dalam bentuk heatmap.
+    """
+    cm = confusion_matrix(y_true, y_pred)
+    plt.figure(figsize=figsize)
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', 
+                xticklabels=labels, yticklabels=labels)
+    plt.xlabel('Predicted')
+    plt.ylabel('Actual')
+    plt.title('Confusion Matrix')
+    plt.tight_layout()
+    return plt
